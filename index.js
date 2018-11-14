2efcd6ee90ab878b6c4c50f9d23d432622731db9
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-13 22:02:43 -0800

    remove unused code

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const program = require("commander");
 const { spawn } = require("child_process");
 const zipPreview = require("./preview/zipPreview").default;
 const uploadPreviews = require("./preview/uploadPreviews").default;
-const importCwd = require("import-cwd");
 
 const currentDirFiles = fs.readdirSync(process.cwd()).filter(file => {
   return ["src", "gatsby-config.js"].includes(file);
@@ -19,10 +18,9 @@ if (currentDirFiles !== 2) {
 
 program
   .option("-p, --preview", "Generate a preview and upload it to WP")
-  .option("-t, --test", "Just testing!")
   .parse(process.argv);
 
-if (!program.preview && !program.test) return program.help();
+if (!program.preview) return program.help();
 
 if (program.preview) {
   console.log("Generating preview site");
@@ -47,8 +45,4 @@ if (program.preview) {
   };
 
   generateAndUploadPreview();
-}
-
-if (program.test) {
-  console.log(importCwd("./gatsby-config.js"));
 }
