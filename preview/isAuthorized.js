c84186857df986315e238ce632f6b80ed230ec64
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-15 23:07:10 -0800

    add authorization check

--- preview/isAuthorized.js
+++ preview/isAuthorized.js
@@ -1,8 +1,22 @@
 const FormData = require("form-data");
+const fetch = require("node-fetch");
 
-const isAuthorized = () => {
+const isAuthorized = async () => {
   console.log("Authorizing..");
-  return true;
+
+  const form = new FormData();
+  form.append(
+    "apikey",
+    "803eaf96984d2fbb793746a4ab8dffc15896cf46b4ffb424d733c8374e520c9c"
+  );
+  form.append("gatsbypress_preview_keycheck", "true");
+
+  let response = await fetch("https://temperance.online", {
+    method: "POST",
+    body: form
+  });
+  let data = await response.json();
+  return data;
 };
 
-exports.default = isAuthorized;
+module.exports = isAuthorized;
