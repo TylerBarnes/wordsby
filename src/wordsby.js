f2453d4a4bb3d758b1b30b59329e4b0d4dcb2a78
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-15 23:52:34 -0800

    update wordsby to work with babel

--- src/wordsby.js
+++ src/wordsby.js
@@ -4,10 +4,14 @@ const isGatsby = require("./functions/isGatsby").default;
 const generateAndUploadPreview = require("./preview/generateAndUploadPreview")
   .default;
 
-if (!isGatsby()) return;
+const wordsby = () => {
+  if (!isGatsby()) return;
 
-program
-  .option("preview", "Generate a preview and upload it to WP")
-  .parse(process.argv);
+  program
+    .option("preview", "Generate a preview and upload it to WP")
+    .parse(process.argv);
 
-return program.preview ? generateAndUploadPreview() : program.help();
+  return program.preview ? generateAndUploadPreview() : program.help();
+};
+
+wordsby();
