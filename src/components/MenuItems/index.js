f95348e46f9a8a8192556768a1581d8857b076f9
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-29 12:00:37 -0800

    remove wordpress url from menu items

--- src/components/MenuItems/index.js
+++ src/components/MenuItems/index.js
@@ -2,56 +2,63 @@ import React from "react";
 import { Link, StaticQuery, graphql } from "gatsby";
 
 export default ({ slug: propSlug, children }) => {
-  return (
-    <>
-      {propSlug ? (
-        <StaticQuery
-          query={graphql`
-            {
-              allWordpressWpApiMenusMenusItems {
-                edges {
-                  node {
-                    slug
-                    items {
-                      title
-                      url
-                      wordpress_id
-                    }
-                  }
+  return propSlug ? (
+    <StaticQuery
+      query={graphql`
+        {
+          wordpressSiteMetadata {
+            url
+          }
+          allWordpressWpApiMenusMenusItems {
+            edges {
+              node {
+                slug
+                items {
+                  title
+                  url
+                  wordpress_id
                 }
               }
             }
-          `}
-          render={({ allWordpressWpApiMenusMenusItems: wpmenu }) => {
-            const menu = wpmenu.edges.filter(
-              ({ node }) => node.slug === propSlug
-            );
+          }
+        }
+      `}
+      render={({
+        allWordpressWpApiMenusMenusItems: wpmenu,
+        wordpressSiteMetadata: { url }
+      }) => {
+        const menu = wpmenu.edges.filter(({ node }) => node.slug === propSlug);
 
-            const items = menu.length > 0 ? menu[0].node.items : false;
+        const items = menu.length > 0 ? menu[0].node.items : false;
 
-            if (children) {
-              return children(items);
-            }
+        // remove the WP site url from menu links.
+        for (const item of items) {
+          item.url = item.url.replace(url, "");
+        }
 
-            return items ? (
-              <div className="">
-                {items.map(item => (
-                  <Link key={`menu-item-${item.wordpress_id}`} to={item.url}>
-                    {item.title}
-                  </Link>
-                ))}
-              </div>
-            ) : (
-              <h2>
-                {propSlug} doesn't return anything. Maybe you have a spelling
-                error?
-              </h2>
-            );
-          }}
-        />
-      ) : (
-        <h2>Add a WP menu slug to return menu items.</h2>
-      )}
-    </>
+        return items ? (
+          children ? (
+            children(items)
+          ) : (
+            <MenuItemStyles>
+              {items.map(item => (
+                <Link key={`menu-item-${item.wordpress_id}`} to={item.url}>
+                  {item.title}
+                </Link>
+              ))}
+            </MenuItemStyles>
+          )
+        ) : (
+          <h2>
+            slug="
+            {propSlug}" doesn't return menu items.
+            <br />
+            Maybe you have a spelling error?
+          </h2>
+        );
+      }}
+    />
+  ) : (
+    <h2>Add a WP menu slug to return menu items.</h2>
   );
 };
