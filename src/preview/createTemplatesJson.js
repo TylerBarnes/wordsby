bf8b44059f623a7f205fe74b8c0e2dcd2008b277
Tyler Barnes <tylerdbarnes@gmail.com>
2018-12-08 15:26:38 -0800

    send just templates to the WP install

--- src/preview/createTemplatesJson.js
+++ src/preview/createTemplatesJson.js
@@ -2,21 +2,24 @@ const fs = require("fs");
 const path = require("path");
 
 const createTemplatesJson = ({ existingTemplateFiles, templatesPath }) => {
-  const trimmedPaths = existingTemplateFiles.map(fullPath =>
-    fullPath.replace(templatesPath + "/", "").replace(/\.js|\.jsx/gi, "")
-  );
+  return new Promise((resolve, reject) => {
+    const trimmedPaths = existingTemplateFiles.map(fullPath =>
+      fullPath.replace(templatesPath + "/", "").replace(/\.js|\.jsx/gi, "")
+    );
 
-  const templateJsonString = JSON.stringify(trimmedPaths);
-  const filepath = path.resolve(`./public/templates.json`);
-  const dir = "./public";
+    const templateJsonString = JSON.stringify(trimmedPaths);
+    const filepath = path.resolve(`./public/templates.json`);
+    const dir = path.resolve("./public");
 
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-  }
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
 
-  // Save templates array to the public folder. to be consumed by WP for template switching and preview urls
-  fs.writeFile(filepath, new Buffer(templateJsonString, "utf8"), err => {
-    if (err) throw err;
+    // Save templates array to the public folder. to be consumed by WP for template switching and preview urls
+    fs.writeFile(filepath, new Buffer.from(templateJsonString, "utf8"), err => {
+      if (err) reject(err);
+      resolve();
+    });
   });
 };
 
--- src/wordsby.js
+++ src/wordsby.js
@@ -7,8 +7,11 @@ const devPreview = require("./preview/devPreview");
 const createTemplatesJson = require("./preview/createTemplatesJson");
 const path = require("path");
 const glob = require("glob");
+const zipPreview = require("./preview/zipPreview");
+const uploadPreviews = require("./preview/uploadPreviews");
+const exec = require("child_process").exec;
 
-const wordsby = () => {
+const wordsby = async () => {
   if (!isGatsby()) return;
 
   program
@@ -22,11 +25,30 @@ const wordsby = () => {
   } else if (program.preview) {
     return generateAndUploadPreview();
   } else if (program.templates) {
+    try {
+      await new Promise((resolve, reject) => {
+        exec("rm -rf " + path.resolve(`./public`), function(err) {
+          // deleted public folder.
+          if (err) reject(err);
+
+          resolve("deleted public folder");
+        });
+      });
+    } catch (err) {
+      throw err;
+    }
+
     const templatesPath = path.resolve(`./src/templates/`);
     let existingTemplateFiles = glob.sync(`${templatesPath}/**/*.js`, {
       dot: true
     });
-    return createTemplatesJson({ existingTemplateFiles, templatesPath });
+    await createTemplatesJson({ existingTemplateFiles, templatesPath });
+    try {
+      await zipPreview();
+    } catch (error) {
+      throw error;
+    }
+    uploadPreviews();
   } else {
     return program.help();
   }
