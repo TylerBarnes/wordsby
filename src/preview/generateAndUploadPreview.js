304120855b84184c6f93b1761c8c642b6a12b0aa
Tyler Barnes <tylerdbarnes@gmail.com>
2018-12-12 12:08:19 -0800

    delete public folder before generating preview build

--- src/preview/generateAndUploadPreview.js
+++ src/preview/generateAndUploadPreview.js
@@ -1,21 +1,32 @@
-const { spawn } = require("child_process");
+const { spawn, exec } = require("child_process");
 const zipPreview = require("./zipPreview");
 const uploadPreviews = require("./uploadPreviews");
 const isAuthorized = require("./isAuthorized");
 const readline = require("readline");
 
-const generateAndUploadPreview = async () => {
-  // console.log(authorized);
-  let authorized = false;
-  try {
-    authorized = await isAuthorized();
-  } catch (e) {
-    throw e;
+const generateAndUploadPreview = async ({ skipUpload = false }) => {
+  if (!skipUpload) {
+    let authorized = false;
+    try {
+      authorized = await isAuthorized();
+    } catch (e) {
+      throw e;
+    }
+    if (authorized !== "success") return;
   }
 
-  if (authorized !== "success") return;
+  const publicPath = `${process.cwd()}/public`;
+  // const cachePath = `${process.cwd()}/.cache`;
+  const rm = exec(`rm -r ${publicPath}`, (err, stdout, stderr) => {
+    if (!!err) {
+      throw `rimraf process exited with error ${err}`;
+    } else {
+      console.log("Deleted public/ directory to build preview");
+    }
+    rm.stdin.end();
+  });
 
-  console.log("Generating preview site");
+  console.log("Generating preview build");
   process.env["WORDSBY_PREVIEW"] = true;
   const gatsbyBuild = spawn("gatsby", ["build", "--prefix-paths"]);
 
@@ -48,12 +59,18 @@ const generateAndUploadPreview = async () => {
       );
     } else {
       console.log("Finished generating previews.");
+
       try {
         await zipPreview();
       } catch (error) {
         throw error;
       }
-      uploadPreviews();
+
+      if (!skipUpload) {
+        uploadPreviews();
+      } else {
+        console.log("Preview zip generated");
+      }
     }
     gatsbyBuild.stdin.end();
   });
